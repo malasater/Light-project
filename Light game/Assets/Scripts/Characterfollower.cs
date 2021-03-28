48f15770429775f5fd48ef3df62e55d22f30b555
malasater <61392769+malasater@users.noreply.github.com>
2021-03-27 21:24:31 -0400

    expanded light movement
    light nows moves back and forth when idle

--- Light game/Assets/Scripts/Characterfollower.cs
+++ Light game/Assets/Scripts/Characterfollower.cs
@@ -6,22 +6,50 @@ public class Characterfollower : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject character;
-    private Vector3 offset;
+    private Vector3 rightoffset;
+    private Vector3 leftoffset;
+    private bool leftflag = false;
     public float speed = 1.0f;
     private Vector3 target;
+    private float initDist;
+    private Vector3 newpos;
     void Start()
     {
-        offset = transform.position - character.transform.position;
+        leftoffset = transform.position - character.transform.position;
+        rightoffset.Set(-(leftoffset.x), leftoffset.y, leftoffset.z);
+        initDist = Vector3.Distance(transform.position, character.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = character.transform.position + offset;
+        if (transform.position == (character.transform.position + leftoffset))
+        {
+            leftflag = false;
+                
+        }
+        if (transform.position == (character.transform.position + rightoffset))
+        {
+            leftflag = true;
+        }
+        if (leftflag)
+        {
+            target = character.transform.position + leftoffset;
 
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+        if (!leftflag)
+        {
+            target = character.transform.position + rightoffset;
+
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+
+       
 
         //transform.position = character.transform.position + offset;
-        
+
     }
+
+   
 }
