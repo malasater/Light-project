e1d333927f2e154fcc7cbb2ab258236fcc6396fe
malasater <61392769+malasater@users.noreply.github.com>
2021-05-04 15:18:41 -0400

    Fixed sword

--- Light game/Assets/Scripts/Sword.cs
+++ Light game/Assets/Scripts/Sword.cs
@@ -7,6 +7,7 @@ public class Sword : MonoBehaviour
     public Collider2D sword;
     //public SpriteRenderer swordsprite;
     private bool rightflag;
+    private bool down;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Sword : MonoBehaviour
         //EventManager.StartListening("flipswordleft", flipSwordLeft);
         //sword.enabled = false;
         rightflag = true;
+        down = false;
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class Sword : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             sword.enabled = true;
+            down = true;
             //transform.Translate(0.5f, -0.9f, 0.0f);
             if (rightflag)
             {
@@ -44,6 +47,7 @@ public class Sword : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             sword.enabled = false;
+            down = false;
             if (rightflag)
             {
                 transform.Rotate(0.0f, 0.0f, 60.0f);
@@ -73,16 +77,37 @@ public class Sword : MonoBehaviour
     {
         Vector3 temp = transform.localScale;
         temp.x = temp.x * -1.0f;
-        transform.localScale = temp;
-        rightflag = !rightflag;
+        if (down)
+        {
+            if (rightflag)
+            {
+                transform.Rotate(0.0f, 0.0f, 60.0f);
+            }
+            if (!rightflag)
+            {
+                transform.Rotate(0.0f, 0.0f, -60.0f);
+            }
+
+            transform.localScale = temp;
+            rightflag = !rightflag;
+            if (rightflag)
+            {
+                transform.Rotate(0.0f, 0.0f, -60.0f);
+            }
+            if (!rightflag)
+            {
+                transform.Rotate(0.0f, 0.0f, 60.0f);
+            }
+
+
+        }
+        else
+        {
+            transform.localScale = temp;
+            rightflag = !rightflag;
+        }
     }
-    void flipSwordLeft()
-    {
-        rightflag = false;
-        //transform.Translate(-1.1f, 0.0f, 0.0f);
-        transform.Rotate(0.0f, 0.0f, 60.0f);
-        
-    }
+
 
 
 }
