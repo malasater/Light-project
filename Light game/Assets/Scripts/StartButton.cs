a2e0e754abe13b59d6d8625f41d8f28a0b274e9b
Marvin <madavis1@davidson.edu>
2021-05-10 07:51:51 -0400

    game completed

--- Light game/Assets/Scripts/StartButton.cs
+++ Light game/Assets/Scripts/StartButton.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 public class StartButton : MonoBehaviour
 {
-    
+    public string sceneName;
     public void startGame()
     {
-        SceneManager.LoadScene("first");
+        SceneManager.LoadScene(sceneName);
     }
-
-
 }
+
+
+
+
